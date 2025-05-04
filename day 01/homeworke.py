bbc93a9e45a89b1189a028b807504906842b095b
aleqsandre123goa <tabatadzealeqsi2010@gmail.com>
2025-05-04 02:41:57 -0700

    commit

--- day 01/homeworke.py
+++ day 01/homeworke.py
@@ -1,20 +1,20 @@
-name="aleqsandre"
-age="14"
-addres="chiatura"
-height="171"
-weight="60"
+name = "aleqsandre"
+age = 14
+addres = "chiatura"
+height = 171
+weight = 60
+print("my name is", name, "i am", age, "years old", "i live in", addres, "i am", height, "cm tall", "i am", weight, "kg")
 
 print(25) # არის ფუნქცია რომელიც შედეგს გვაძლევს ტერმინალში
 #string -  ბრჭყალებში მოქცეული სიმბოლოები
 #integer - ყველა მთელი რიცხვი მინუს უსასრულობიდან პლიუს უსასრულობამდე
 #float - ყველა ათწილადი
-#name - არის ცვლადის სახელი "ალექსი" - ბნისვნელობა
-# - მინიჭების ოპერატორი
+print("aleqsi")
+print(14)
+print(2.5)
+#name - არის ცვლადის სახელი "ალექსი" - მნიშვნელობა
+# = - მინიჭების ოპერატორი
 #ctrl+? - შორთქათ რომელიც აკომენტარებს მრავალხაზიან კოდს
 #declaration - დეკლარაცია ცვლადის სახელის შექმნა
 #inicialization - ინიციალიზება მნიშვნელობის მინიჭებას ცვლადზე
 #reinicialization - რეინიციალიზება არის იგივე ცვლადის მნიშვნელობის განახლება
-
-
-
-
