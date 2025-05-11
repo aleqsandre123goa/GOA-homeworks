82c1f0eec2885229ae74a8e18b199eac340adb90
aleqsandre123goa <tabatadzealeqsi2010@gmail.com>
2025-05-11 07:12:43 -0700

    commit

--- day 02/classwork/classwork.py
+++ day 02/classwork/classwork.py
@@ -1,11 +1,5 @@
-
-print(5+6)
-print(7-3)
-print(4*6)
-print(6/3)
-print(5/2)
-
-
-
-
-
+print(5 + 6)
+print(7 - 3)
+print(4 * 6)
+print(6 / 3)
+print(5 // 2)
---
+++ day 02/homework/homework.py
@@ -0,0 +1,19 @@
+number1 = 10
+number2 = 5
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 // number2)
+#////////////////////////////////////////////////////////////
+birth_year = 2010
+current_year = 2025
+age = current_year - birth_year
+print(age)
+#////////////////////////////////////////////////////////////
+number1 = 10
+number2 = 5
+number3 = 9
+number4 = 23
+number5 = 15
+print((number1 + number2 + number3 + number4 + number5) / 5)
