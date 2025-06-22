41fad31f1da8fff021554de9b1a72e4a91e43cd7
aleqsandre123goa <tabatadzealeqsi2010@gmail.com>
2025-06-22 01:59:48 -0700

    commit

---
+++ day 08/classwork/classwork.py
@@ -0,0 +1,6 @@
+input("giorgi how old are you:")
+
+input("luka how old are you:")
+
+
+
---
+++ day 08/homework/homework.py
@@ -0,0 +1,14 @@
+#string-ბრჭყალებში მოქცეული სიმბოლო
+#integer-ყველა მთელი რიცხვი
+#float-ყველა ათწილადი
+#type-ამოწმებს მონაცემთა ტიპებს
+name = "aleqsi"
+age = 14
+height = 1.73
+sport = "futball"
+weight = 63
+print( type(name) )
+print( type(age) )
+print( type(height) )
+print( type(sport) )
+print( type(weight) )
